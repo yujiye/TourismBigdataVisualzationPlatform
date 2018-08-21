60268a3d9d53e168dc1188adc0dad477e651537a
zqw961025 <2384236461@qq.com>
2018-08-21 19:15:52 +0800

    idea test pull1

--- src/src/main/java/com/gxu/tbvp/controller/HomeController.java
+++ src/src/main/java/com/gxu/tbvp/controller/HomeController.java
@@ -48,11 +48,6 @@ public class HomeController {
         }
     }
 
-    @RequestMapping("/rolesPage")
-    public String rolesPage(){
-        return "role/roles";
-    }
-
     @RequestMapping("/resourcesPage")
     public String resourcesPage(){
         return "resources/resources";
